3ad264456e9e84b3cd02646c60cbbb47267ca177
isakhanian <karine.isakhanian@gmail.com>
2017-04-26 22:50:52 +0300

    Выделен вспомогательный класс для теста на добавление контактов

--- fixture/appliance.py
+++ fixture/appliance.py
@@ -1,89 +1,19 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
+from fixture.contacts import ContactsHelper
 
 class Appliance:
 
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
+        self.contacts = ContactsHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_contact_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def add_new_user(self, group):
-        wd = self.wd
-        self.open_contact_page()
-        # add new user
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group.name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(group.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(group.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group.companyaddress)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(group.homenumber)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(group.mobilenumber)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(group.worknumber)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(group.faxnumber)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(group.email)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(group.homeaddress)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(group.secondhomenumber)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(group.notes)
-        # enter
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.check_list()
-
-
-    def check_list(self):
-        wd = self.wd
-        # check list
-        wd.find_element_by_link_text("home").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from fixture.appliance import Application
+from fixture.appliance import Appliance
 from model.contacts import Contacts
 
 
 @pytest.fixture
 def app(request):
-    fixture = Application()
+    fixture = Appliance()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.add_new_user(Contacts(name="Иван", middlename="Иванович", lastname="Иванов", nickname="Ван", company="Галактика", companyaddress="Планетная улица, 1", homenumber="111-11-11",
+    app.session.login(username="admin", password="secret")
+    app.contacts.add_new_user(Contacts(name="Иван", middlename="Иванович", lastname="Иванов", nickname="Ван", company="Галактика", companyaddress="Планетная улица, 1", homenumber="111-11-11",
                           mobilenumber="+7 (111) 111-11-11", worknumber="999-99-99", faxnumber="888-88-88", email="ivan@ivan.ru",
                           homeaddress="Галактическая улица, 2", secondhomenumber="555-55-55", notes="No"))
-    app.logout()
+    app.session.logout()
 
 def test_add_another_contact(app):
-    app.login(username="admin", password="secret")
-    app.add_new_user(Contacts(name="Федор", middlename="Федорович", lastname="Федоров", nickname="Фед", company="Вселенная", companyaddress="Красивая улица, 1", homenumber="222-22-22",
+    app.session.login(username="admin", password="secret")
+    app.contacts.add_new_user(Contacts(name="Федор", middlename="Федорович", lastname="Федоров", nickname="Фед", company="Вселенная", companyaddress="Красивая улица, 1", homenumber="222-22-22",
                           mobilenumber="+7 (222) 222-22-22", worknumber="222-22-22", faxnumber="222-22-22", email="fedor@fedor.ru",
                           homeaddress="Галактическая улица, 4", secondhomenumber="555-55-55", notes="No"))
-    app.logout()
+    app.session.logout()
 
