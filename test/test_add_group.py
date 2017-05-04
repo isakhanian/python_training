0b80dc47daa450da55fa3ee09264a3ee411a8567
isakhanian <karine.isakhanian@gmail.com>
2017-05-04 22:40:23 +0300

    Не запускается приложение

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,6 +5,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="new", header="newheader", footer="newfooter"))
+    app.session.logout()
 
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
