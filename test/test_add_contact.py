810d2bcd6382945282af21a2611b4c3dd81a4f3c
isakhanian <karine.isakhanian@gmail.com>
2017-05-04 07:09:18 +0300

    Добавлена проверка в фикстуру

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,16 +4,12 @@ from model.contacts import Contacts
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contacts.add_new_user(Contacts(name="Иван", middlename="Иванович", lastname="Иванов", nickname="Ван", company="Галактика", companyaddress="Планетная улица, 1", homenumber="111-11-11",
                           mobilenumber="+7 (111) 111-11-11", worknumber="999-99-99", faxnumber="888-88-88", email="ivan@ivan.ru",
                           homeaddress="Галактическая улица, 2", secondhomenumber="555-55-55", notes="No"))
-    app.session.logout()
 
 def test_add_another_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contacts.add_new_user(Contacts(name="Федор", middlename="Федорович", lastname="Федоров", nickname="Фед", company="Вселенная", companyaddress="Красивая улица, 1", homenumber="222-22-22",
                           mobilenumber="+7 (222) 222-22-22", worknumber="222-22-22", faxnumber="222-22-22", email="fedor@fedor.ru",
                           homeaddress="Галактическая улица, 4", secondhomenumber="555-55-55", notes="No"))
-    app.session.logout()
 
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 
 def test_delete_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contacts.delete_first_contact()
-    app.session.logout()
