7cdff8a7053ea23ebd7e8891409ebfadec929c2b
isakhanian <karine.isakhanian@gmail.com>
2017-05-06 11:09:39 +0300

    Удаление контакта с предварительной проверкой

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -12,52 +12,52 @@ class ContactsHelper:
         # check list
         wd.find_element_by_link_text("home").click()
 
-    def add_new_user(self, group):
+    def add_new_user(self, contacts):
         wd = self.app.wd
         self.open_contact_page()
         # add new user
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group.name)
+        wd.find_element_by_name("firstname").send_keys(contacts.name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(group.middlename)
+        wd.find_element_by_name("middlename").send_keys(contacts.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group.lastname)
+        wd.find_element_by_name("lastname").send_keys(contacts.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(group.nickname)
+        wd.find_element_by_name("nickname").send_keys(contacts.nickname)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group.company)
+        wd.find_element_by_name("company").send_keys(contacts.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group.companyaddress)
+        wd.find_element_by_name("address").send_keys(contacts.companyaddress)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(group.homenumber)
+        wd.find_element_by_name("home").send_keys(contacts.homenumber)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(group.mobilenumber)
+        wd.find_element_by_name("mobile").send_keys(contacts.mobilenumber)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(group.worknumber)
+        wd.find_element_by_name("work").send_keys(contacts.worknumber)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(group.faxnumber)
+        wd.find_element_by_name("fax").send_keys(contacts.faxnumber)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(group.email)
+        wd.find_element_by_name("email").send_keys(contacts.email)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(group.homeaddress)
+        wd.find_element_by_name("address2").send_keys(contacts.homeaddress)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(group.secondhomenumber)
+        wd.find_element_by_name("phone2").send_keys(contacts.secondhomenumber)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(group.notes)
+        wd.find_element_by_name("notes").send_keys(contacts.notes)
         # enter
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.check_list()
@@ -101,7 +101,10 @@ class ContactsHelper:
         wd.switch_to_alert().accept()
         self.check_list()
 
-
+    def count(self):
+        wd = self.app.wd
+        self.open_contact_page()
+        return len(wd.find_elements_by_name("selected[]"))
 
 
 
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from model.contacts import Contacts
+
 def test_delete_first_contact(app):
+    if app.contacts.count() == 0:
+        app.contacts.add_new_user(Contacts(name="Иван", middlename="Иванович", lastname="Иванов", nickname="Ван", company="Галактика", companyaddress="Планетная улица, 1", homenumber="111-11-11",
+                          mobilenumber="+7 (111) 111-11-11", worknumber="999-99-99", faxnumber="888-88-88", email="ivan@ivan.ru",
+                          homeaddress="Галактическая улица, 2", secondhomenumber="555-55-55", notes="No"))
     app.contacts.delete_first_contact()
