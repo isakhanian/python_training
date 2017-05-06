694826243d77b5af0f112f85a31949253f4f7014
isakhanian <karine.isakhanian@gmail.com>
2017-05-06 13:46:02 +0300

    Скорректированы вспомогательные методы для контактов и добавлена проверка перехода на страницу

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -9,94 +9,66 @@ class ContactsHelper:
 
     def check_list(self):
         wd = self.app.wd
-        # check list
+        if wd.current_url.endswith("/index.php") and len(wd.find_elements_by_xpath("//input[@value='Delete']")) > 0:
+            return
         wd.find_element_by_link_text("home").click()
+
+    def change_field(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_contact_form(self, contacts):
+        wd = self.app.wd
+        self.change_field("name", contacts.name)
+        self.change_field("middlename", contacts.middlename)
+        self.change_field("lastname", contacts.lastname)
+        self.change_field("nickname", contacts.nickname)
+        self.change_field("company", contacts.company)
+        self.change_field("address", contacts.companyaddress)
+        self.change_field("home", contacts.homenumber)
+        self.change_field("mobile", contacts.mobilenumber)
+        self.change_field("work", contacts.worknumber)
+        self.change_field("fax", contacts.faxnumber)
+        self.change_field("email", contacts.email)
+        self.change_field("address2", contacts.homeaddress)
+        self.change_field("phone2", contacts.secondhomenumber)
+        self.change_field("notes", contacts.notes)
+
+
+    def select_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def add_new_user(self, contacts):
         wd = self.app.wd
         self.open_contact_page()
         # add new user
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contacts.name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contacts.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contacts.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contacts.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contacts.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contacts.companyaddress)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contacts.homenumber)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contacts.mobilenumber)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contacts.worknumber)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contacts.faxnumber)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contacts.email)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contacts.homeaddress)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contacts.secondhomenumber)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contacts.notes)
+        self.fill_contact_form(contacts)
         # enter
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.check_list()
 
-
-    def modify_first_contact(self, contacts):
+    def modify_first_contact(self, new_contact_data):
         wd = self.app.wd
         self.check_list()
         # select first contact
         # modify first contact
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_contact()
         wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contacts.homeaddress)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contacts.homenumber)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contacts.mobilenumber)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contacts.worknumber)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contacts.faxnumber)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contacts.email)
+        self.fill_contact_form(new_contact_data)
         wd.find_element_by_name("update").click()
         self.check_list()
+
 
     def delete_first_contact(self):
         wd = self.app.wd
         self.check_list()
         # select first contact
         # delete first contact
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_contact()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
         self.check_list()
--- fixture/group.py
+++ fixture/group.py
@@ -46,26 +46,6 @@ class GroupHelper:
         self.return_to_groups_page()
 
 
-    def edit_first_group(self, gredit):
-        wd = self.app.wd
-        self.open_group_page()
-        # select first group
-        #edit first group
-        self.select_first_group()
-        self.edit_group()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(gredit.edname)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(gredit.edheader)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(gredit.edfooter)
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
-
-
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
