3e598e35dc093736a74254bda82fecf712915aae
isakhanian <karine.isakhanian@gmail.com>
2017-05-09 23:01:19 +0300

    Добавлено сравнение списков в тесты по группам

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,8 +4,13 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="new", header="newheader", footer="newfooter"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
