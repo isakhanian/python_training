effc69541358b003cdd7e25d8c0492d49142788f
isakhanian <karine.isakhanian@gmail.com>
2017-05-09 16:27:55 +0300

    Упрощены тесты для удаления групп и контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,11 +4,7 @@ from model.contacts import Contacts
 
 def test_delete_first_contact(app):
     if app.contacts.count() == 0:
-        print("no any contacts")
         app.contacts.add_new_user(Contacts(name="Иван", middlename="Иванович", lastname="Иванов", nickname="Ван", company="Галактика", companyaddress="Планетная улица, 1", homenumber="111-11-11",
                           mobilenumber="+7 (111) 111-11-11", worknumber="999-99-99", faxnumber="888-88-88", email="ivan@ivan.ru",
                           homeaddress="Галактическая улица, 2", secondhomenumber="555-55-55", notes="No"))
-        app.contacts.delete_first_contact()
-    else:
-        print("contacts are here")
-        app.contacts.delete_first_contact()
+    app.contacts.delete_first_contact()
