705b3b9a30355ce5195f01cb7ef90e359fb9d923
isakhanian <karine.isakhanian@gmail.com>
2017-05-22 21:52:30 +0300

    Параметризация браузера, пароля и url

--- conftest.py
+++ conftest.py
@@ -9,12 +9,13 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     base_url = request.config.getoption("--baseUrl")
+    password = request.config.getoption("--password")
     if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
+        fixture = Application(browser=browser, base_url=base_url, password=password)
     else:
         if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url, password=password)
+    fixture.session.ensure_login(username="admin", password=password)
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -28,3 +29,4 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--password", action="store", default="secret")
--- fixture/application.py
+++ fixture/application.py
@@ -5,7 +5,7 @@ from fixture.contacts import ContactsHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, base_url, password):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -18,6 +18,7 @@ class Application:
         self.group = GroupHelper(self)
         self.contacts = ContactsHelper(self)
         self.base_url = base_url
+        self.password = password
 
     def is_valid(self):
         try:
